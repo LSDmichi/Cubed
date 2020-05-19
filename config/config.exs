cc40c417b3f5c70f17e8bb72621303114b6df16f
higashi masamichi <masa@higashi-no-MacBook-Air.local>
2020-05-19 14:36:35 +0900

    test

---
+++ config/config.exs
@@ -0,0 +1,38 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :app,
+  ecto_repos: [App.Repo]
+
+# Configures the endpoint
+config :app, AppWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "lBFHBBMX9A/bNatwBatRz2A8y8mgGeaVWhMCko742emL9YL2BHy0rDgz8UXGssgd",
+  render_errors: [view: AppWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: App.PubSub,
+  live_view: [signing_salt: "cYAQNMVP"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Configures Elixir's Auto lint
+config :mix_test_watch,
+  tasks: [
+    "test",
+    "credo",
+  ]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
